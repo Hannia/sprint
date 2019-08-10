ff045f38723e75ab60cd89b758e12bd9c28902a7
Annia <anniajesicagomezblanco@gmail.com>
2019-08-10 01:23:28 +0100

    added home and episodes page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Home from "./components/home";
+import Episodes from "./components/episodes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route, Redirect, Switch } from "react-router-dom";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <main className="container">
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/episodes" component={Episodes} />
+            <Redirect to="/" exact to="/home" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
