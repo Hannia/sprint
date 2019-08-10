9cd2c0257aa929ba7506317cb16a42d0957a5e73
Annia <anniajesicagomezblanco@gmail.com>
2019-08-10 01:46:53 +0100

    added filter episodes

--- src/components/episodes.jsx
+++ src/components/episodes.jsx
@@ -1,25 +1,55 @@
 import React, { Component } from "react";
 import { getEpisodes } from "../services/episodesService";
+import _ from "lodash";
 
 class Episodes extends Component {
   state = {
     episodes: [],
+    allEpisodes: [],
     limitEpisodes: 6,
-    offsetEpisodes: 0
+    offsetEpisodes: 0,
+    displayFilters: false,
+    seasons: ["1", "2", "3", "4", "5"]
   };
 
   componentDidMount = async () => {
     const episodes = await getEpisodes();
-    this.setState({ episodes });
+    this.setState({ episodes, allEpisodes: episodes });
   };
 
-  // filterEpisodes = () => {
-  //   const { episodes } = this.state;
-  // };
+  filterEpisodesBySeason = season => {
+    const { allEpisodes } = this.state;
+    const filteredEpisodes = _.filter(allEpisodes, episode => {
+      return episode.season === season;
+    });
+
+    this.setState({ episodes: filteredEpisodes });
+  };
+
+  toggleFilters = () => {
+    this.setState({ displayFilters: !this.state.displayFilters });
+  };
 
   render() {
     return (
       <div className="container">
+        <div className="dropdown show">
+          <a className="btn btn-secondary dropdown-toggle" onClick={this.toggleFilters}>
+            Seassons
+          </a>
+
+          <div style={{ display: this.state.displayFilters ? "block" : "none" }} className="dropdown-menu">
+            {this.state.seasons.map(season => (
+              <a
+                onClick={() => {
+                  this.filterEpisodesBySeason(season);
+                }}
+                className="dropdown-item"
+              >{`Season ${season}`}</a>
+            ))}
+          </div>
+        </div>
+
         {this.state.episodes.map(episode => (
           <div className="episode-container" key={episode.episode_id}>
             <strong>Title: {episode.title}</strong>
